7171830dc57b1da07125850b5c9e98f9236d0063
Christopher Palte <palte.christopher@gmail.com>
2024-08-23 10:53:22 +0200

    add Visitor Counter

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("test");
+var n = sessionStorage.getItem("on_load_counter");
+
+if (n === null) {
+  n = 0;
+}
+n++;
+
+console.log(n);
+
+sessionStorage.setItem("on_load_counter", n);
+
+function updateHTML(elmId, value) {
+    var elem = document.getElementById(elmId);
+    if(typeof elem !== 'undefined' && elem !== null) {
+      elem.innerHTML = value;
+    }
+  }
+
+document.getElementById("counter").innerHTML = n;
